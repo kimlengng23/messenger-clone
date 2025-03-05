360f5f3a3f9cacd872e7b5483b84aac2a5c0b7cb
kimlengng23 <kimlengng23@gmail.com>
2025-03-04 21:33:25 -0800

    fixed

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import NextAuth, { AuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -49,5 +49,3 @@ export const authOptions: AuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
 }
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };

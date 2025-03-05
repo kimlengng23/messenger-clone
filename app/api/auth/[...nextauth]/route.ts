3482f48bc08c287398570c6b4127bde0b44ef903
kimlengng23 <kimlengng23@gmail.com>
2025-03-04 22:04:22 -0800

    fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import { authOptions } from "@/app/libs/auth";
 
-const handler = NextAuth(authOptions);
-export const GET = handler.handlers.GET;
-export const POST = handler.handlers.POST;
+const handlers = NextAuth(authOptions);
+export { handlers as GET, handlers as POST }
--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -4,8 +4,8 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import prisma from "@/app/libs/prismadb";
+
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
